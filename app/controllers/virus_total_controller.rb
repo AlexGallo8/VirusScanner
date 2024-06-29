de222af49172b460162c25c33ee6e8ea6daa39bb
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-06-29 15:57:43 +0200

    scan url nella home

--- app/controllers/virus_total_controller.rb
+++ app/controllers/virus_total_controller.rb
@@ -16,8 +16,10 @@ class VirusTotalController < ApplicationController
       file_id = upload_file(file_path)
       sleep(5) until get_analysis_result(file_id).size > 0
       @results = get_analysis_result(file_id)
+    elsif params[:url].present?
+      scan_url
     else
-      flash[:alert] = "Per favore, seleziona un file da scansionare."
+      flash[:alert] = "Per favore, seleziona un file da scansionare o inserisci un URL"
       redirect_to virus_total_path
     end
   end
@@ -35,6 +37,30 @@ class VirusTotalController < ApplicationController
     request.set_form form_data, 'multipart/form-data'
     
     response = Net::HTTP.start(url.hostname, url.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+    
+    JSON.parse(response.body)['data']['id']
+  end
+
+  def scan_url
+    url = params[:url]
+    url_id = submit_url(url)
+    sleep 15 # Attendi 15 secondi prima di controllare i risultati
+    @results = get_analysis_result(url_id)
+    render :scan
+  end
+
+  def submit_url(url)
+    api_url = URI("#{BASE_URL}/urls")
+    
+    request = Net::HTTP::Post.new(api_url)
+    request["accept"] = 'application/json'
+    request["x-apikey"] = API_KEY
+    request["content-type"] = 'application/x-www-form-urlencoded'
+    request.body = "url=#{URI.encode_www_form_component(url)}"
+    
+    response = Net::HTTP.start(api_url.hostname, api_url.port, use_ssl: true) do |http|
       http.request(request)
     end
     

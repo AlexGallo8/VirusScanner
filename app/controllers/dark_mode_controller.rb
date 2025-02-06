fe65aefb0decbfacd95e00b176544da29e09b5be
AlexGallo8 <alexmaone8@gmail.com>
2025-02-07 00:47:50 +0100

    snapshot2501070047 dark mode al primo switch

--- app/controllers/dark_mode_controller.rb
+++ app/controllers/dark_mode_controller.rb
@@ -1,6 +1,8 @@
 class DarkModeController < ApplicationController
     def toggle
-        cookies[:dark_mode] = (cookies[:dark_mode] != "true").to_s
+        # Explicitly convert to boolean and then back to string
+        @is_dark = cookies[:dark_mode] != "true"
+        cookies[:dark_mode] = @is_dark.to_s
         
         respond_to do |format|
             format.js { render layout: false }

81797d75cd63695ccd3d9646d37876cd91fa049b
AlexGallo8 <alexmaone8@gmail.com>
2025-02-13 09:13:01 +0100

    backup1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,9 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   
   def new
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def create
